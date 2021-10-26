3bfa944eb2a343bcac28293b08cf09b9258110e0
Arthur <whackon@163.com>
2021-10-26 18:05:38 +0800

    创建 witmed-registry 项目

---
+++ WitmedProject/witmed-registry/src/main/java/com/whackon/witmed/WitmedRegistryStarter.java
@@ -0,0 +1,19 @@
+package com.whackon.witmed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * <b>智慧医疗信息平台 - 注册中心启动类</b>
+ * @author Arthur
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class WitmedRegistryStarter {
+	public static void main(String[] args) {
+		SpringApplication.run(WitmedRegistryStarter.class, args);
+	}
+}
